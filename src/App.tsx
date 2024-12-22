a5db5edf0a75cffc83275bf61c0074dc1d0dff49
rrirrirr <drpihl@gmail.com>
2024-12-22 21:49:14 +0100

    add socket connection

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,26 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Candle from "./Candle";
 import { Suspense } from "react";
+import { useSocket } from "./hooks/useSocket";
 
 function App() {
+  const { connected, candleState, userCount, toggleCandle } = useSocket(
+    import.meta.env.SERVER_URL || "http://localhost:3000",
+  );
+
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
+      <div className="mb-4">
+        <div>Connection status: {connected ? "Connected" : "Disconnected"}</div>
+        <div>Users in room: {userCount}</div>
+        <div>Candle state : {candleState}</div>
+        <button
+          onClick={toggleCandle}
+          className="px-4 py-2 bg-blue-500 text-white rounded"
+        >
+          {candleState ? "Turn Off Candle" : "Turn On Candle"}
+        </button>
+      </div>
       <Suspense fallback={<div>Loading...</div>}>
         <Canvas camera={{ position: [0, 0, 5] }}>
           <color attach="background" args={["#f0f0f0"]} />
