1be564370f497e81cc12e30a0099ee2c833f9048
julstaken <jul@julstake.jul>
2024-12-23 03:47:39 +0000

    performance changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    hmr: false,
+    watch: {
+      usePolling: false
+    }
+  },
+  optimizeDeps: {
+    force: true
+  },
+  build: {
+    sourcemap: false,
+    cssCodeSplit: false
+  },
   assetsInclude: ['**/*.gltf', '**/*.glb'],
 })
